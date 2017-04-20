f518ae12eec2193523a3a6a558228a29fb5a9807
Viktor Sapunkov <viktor.sapunkov@gmail.com>
2017-04-20 12:09:25 +0300

    exception handling improvements (made content negotiation-friendly)

--- url-shortener/src/main/java/com/infobip/urlshortener/config/GlobalControllerExceptionHandler.java
+++ url-shortener/src/main/java/com/infobip/urlshortener/config/GlobalControllerExceptionHandler.java
@@ -4,10 +4,7 @@ import com.infobip.urlshortener.model.errors.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
@@ -17,6 +14,7 @@ public class GlobalControllerExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     @ResponseStatus(value = HttpStatus.SERVICE_UNAVAILABLE)
     @ResponseBody
+    @RequestMapping(produces = "application/json")
     public ErrorResponse catchAll(Exception e) {
         LOGGER.error("Service unavailable", e);
         return new ErrorResponse(String.valueOf(HttpStatus.SERVICE_UNAVAILABLE), String.valueOf(e));
